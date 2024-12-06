c03ced1dc765d5f16b3f9b41c01e51d40022ec04
andrey <pa24@yandex.ru>
2024-12-06 03:19:51 +0300

    more refactor

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -8,7 +8,7 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-var bannedWords = []string{"упал", "реклама", "кроватки", "разбушевавшейся"}
+var bannedWords = []string{"упал", "реклама", "кроватки", "разбушевавшейся", "готовить"}
 
 // StartBot инициализирует и запускает бота
 func StartBot(cfg *config.Config) error {
@@ -17,7 +17,7 @@ func StartBot(cfg *config.Config) error {
 		return err
 	}
 
-	bot.Debug = true
+	bot.Debug = false
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
@@ -26,13 +26,28 @@ func StartBot(cfg *config.Config) error {
 
 	mediaGroupCache := make(map[string]bool)
 	notificationSent := make(map[string]bool)
+	deletedID := make(map[int]bool)
 
 	for update := range updates {
 		if update.Message != nil {
 			if update.Message.ForwardFrom != nil || update.Message.ForwardFromChat != nil {
-				prepareToDelete(bot, update.Message, mediaGroupCache)
-				prepareNotification(bot, update.Message, notificationSent)
+				isNegative := wordFilter(update.Message)
 
+				if isNegative {
+					deleteMessage(bot, update.Message)
+					deletedID[update.Message.MessageID] = true
+					if update.Message.MediaGroupID != "" {
+						mediaGroupCache[update.Message.MediaGroupID] = true
+					}
+				}
+				exist := checkForMediaGroup(update.Message, mediaGroupCache, deletedID)
+				if exist {
+					deleteMediaGroup(bot, update.Message, mediaGroupCache)
+
+				}
+				if isNegative || exist {
+					prepareNotification(bot, update.Message, notificationSent)
+				}
 			}
 		}
 	}
@@ -40,26 +55,48 @@ func StartBot(cfg *config.Config) error {
 	return nil
 }
 
-func prepareNotification(bot *tgbotapi.BotAPI, message *tgbotapi.Message, notificationSent map[string]bool) {
-	if !notificationSent[message.MediaGroupID] {
-		sendNotification(bot, message)
-		notificationSent[message.MediaGroupID] = true
+func deleteMediaGroup(bot *tgbotapi.BotAPI, message *tgbotapi.Message, mediaGroupCache map[string]bool) {
+	if mediaGroupCache[message.MediaGroupID] {
+		deleteMessage(bot, message)
 	}
 }
 
-func prepareToDelete(bot *tgbotapi.BotAPI, message *tgbotapi.Message, cache map[string]bool) {
-	mediaGroupID := message.MediaGroupID
+func checkForMediaGroup(message *tgbotapi.Message, mediaGroupCache map[string]bool, deletedID map[int]bool) bool {
+	if message.MediaGroupID != "" && mediaGroupCache[message.MediaGroupID] {
+		if !deletedID[message.MessageID] {
+			return true
+		}
+	}
+	return false
+}
+
+func prepareNotification(bot *tgbotapi.BotAPI, message *tgbotapi.Message, notificationSent map[string]bool) {
+	if message.MediaGroupID != "" {
+		if !notificationSent[message.MediaGroupID] {
+			sendNotification(bot, message)
+			notificationSent[message.MediaGroupID] = true
+			return
+		}
+	} else {
+		sendNotification(bot, message)
+		return
+	}
+}
+
+func wordFilter(message *tgbotapi.Message) bool {
+	var text string
+	if message.Caption != "" {
+		text = message.Caption
+	} else {
+		text = message.Text
+	}
+
 	// Если есть запрещённые слова, удаляем сообщение
-	var containsWord bool
-	if mediaGroupID != "" {
-		// Проверяем или обновляем статус группы
-		//todo отделить проверку слов от проверки кеша. Проверка кеша должна быть внутри
-		containsWord = containsBannedWord(message.Caption) || cache[mediaGroupID]
-		cache[mediaGroupID] = containsWord
-	}
-	if containsWord {
-		deleteMessage(bot, message)
-	}
+	var negativeWord bool
+	// Проверяем или обновляем статус групп
+	negativeWord = containsBannedWord(text)
+
+	return negativeWord
 }
 
 func containsBannedWord(text string) bool {
