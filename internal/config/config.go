85b730b9c173c810723e710a360df24e0750eeb8
andrey <pa24@yandex.ru>
2024-12-09 01:01:40 +0300

    add banned words.json to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,18 @@
 package config
 
 import (
+	"encoding/json"
 	"errors"
+	"log"
 	"os"
 )
 
 type Config struct {
 	TelegramToken string
+}
+
+type WordConfig struct {
+	BannedWords []string `json:"banned_words"`
 }
 
 // LoadConfig загружает конфигурацию из переменных окружения или других источников
@@ -19,4 +25,25 @@ func LoadConfig() (*Config, error) {
 	return &Config{
 		TelegramToken: token,
 	}, nil
+}
+
+func LoadBannedWords(filepath string) ([]string, error) {
+	// Проверяем, существует ли файл
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if cerr := file.Close(); cerr != nil {
+			log.Printf("Failed to close file %s: %v", filepath, cerr)
+		}
+	}()
+
+	var config WordConfig
+	if err := json.NewDecoder(file).Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return config.BannedWords, nil
 }
